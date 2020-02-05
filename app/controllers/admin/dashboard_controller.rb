214f54b1ad5c623f5f2928bcfc90f0aae27b16c3
Jamie <jamiekaram16@gmail.com>
2020-02-05 22:09:13 +0000

    added admin featues

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with :name => ENV['secret_user'], :password => ENV['secret_password']
 
   def show
-
+    @products = Product.all
+    @category = Category.all
   end
+
 end
